050b52d411ef094f64025591031a4215a5843fed
Travis Jungroth Morton <Jungroth@gmail.com>
2015-04-08 13:20:15 -0700

    Add init.py

--- site.py
+++ site.py
@@ -8,20 +8,24 @@ def index():
 
 @app.route('/briefing')
 def briefing():
+    if request.args:
+        try:
+            location = (float(request.args['latitude']), float(request.args['longitude']))
 
-    location = (float(request.args['latitude']), float(request.args['longitude']))
+            API_KEY = os.getenv('FORECAST_API_KEY')
+            distance = 10
 
-    API_KEY = os.getenv('FORECAST_API_KEY')
-    distance = 10
+            nearby_tfrs = tfr_search(location, distance)
+            nearby_airports = nearby_airports_finder(location, distance)
+            response = forecastio.load_forecast(API_KEY, location[0], location[1])
 
-    nearby_tfrs = tfr_search(location, distance)
-    nearby_airports = nearby_airports_finder(location, distance)
-    response = forecastio.load_forecast(API_KEY, location[0], location[1])
+            weather = Weather(response)
 
-    weather = Weather(response)
-
-    return render_template('briefing.html', distance=distance, airports=nearby_airports, tfrs = nearby_tfrs, weather = weather)
-
+            return render_template('briefing.html', distance=distance, airports=nearby_airports, tfrs = nearby_tfrs, weather = weather)
+        except ValueError:
+            return render_template('index.html')
+    else:
+        return render_template('index.html')
 
 if __name__=='__main__':
     app.run(debug=True)
