e2f10a4e9fb84154e5aa85c44a0229aacb9c65b1
Travis Jungroth Morton <Jungroth@gmail.com>
2015-05-16 15:50:47 -0700

    Change the tfr saver to ignore any TFRs that raise attribute errors.

--- __init__.py
+++ __init__.py
@@ -17,10 +17,18 @@ def briefing():
             API_KEY = os.getenv('FORECAST_API_KEY')
             distance = 10
 
+            #Failsafe to kill the app if it gets too popular (paying for API calls)
+            if(os.path.exists('kill')):
+                return render_template('index.html')
+
             nearby_tfrs = dronewx.tfr_search(location, distance)
             nearby_airports = dronewx.nearby_airports_finder(location, distance)
             response = forecastio.load_forecast(API_KEY, location[0], location[1])
 
+            max_calls_per_day = 20000
+            if(int(response.http_headers['X-Forecast-API-Calls']) > max_calls_per_day):
+                with open('kill', 'w') as f:
+                    pass
             weather = Weather(response)
 
             return render_template('briefing.html', distance=distance, airports=nearby_airports, tfrs = nearby_tfrs, weather = weather)
--- test.py
+++ test.py
@@ -3,4 +3,6 @@ import forecastio, os
 API_KEY = os.getenv('FORECAST_API_KEY')
 response = forecastio.load_forecast(API_KEY, 38.2889, -122.4589)
 
-print(response.offset())
+if(int(response.http_headers['X-Forecast-API-Calls']) > 38):
+    with open('kill', 'w') as f:
+        pass
