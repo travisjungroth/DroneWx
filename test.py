ae7b97c56ef24b35c2612d6c94daf543cfa692e1
Travis Jungroth Morton <Jungroth@gmail.com>
2015-05-16 16:16:04 -0700

    Revert "Change the tfr saver to ignore any TFRs that raise attribute errors."
    This reverts commit e2f10a4e9fb84154e5aa85c44a0229aacb9c65b1.

--- test.py
+++ test.py
@@ -3,6 +3,4 @@ import forecastio, os
 API_KEY = os.getenv('FORECAST_API_KEY')
 response = forecastio.load_forecast(API_KEY, 38.2889, -122.4589)
 
-if(int(response.http_headers['X-Forecast-API-Calls']) > 38):
-    with open('kill', 'w') as f:
-        pass
+print(response.offset())
