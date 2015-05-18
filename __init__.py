5ccde464801aca2b7ebafb8a31d47e39dc57ed44
Travis Jungroth Morton <Jungroth@gmail.com>
2015-05-17 23:38:11 -0700

    Add about page

--- __init__.py
+++ __init__.py
@@ -29,5 +29,8 @@ def briefing():
     else:
         return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')        
 if __name__=='__main__':
     app.run(debug=True)
